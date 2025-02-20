7131a14185cad7bc8baab7db3d1dcadcf183d771
Natalya Nunes <nunes.natalya23@gmail.com>
2025-02-20 08:04:58 -0300

    feat: add initial components

---
+++ src/app/products/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+const ProductPage = () => {
+    return ( 
+        <div>
+            <h1 className="text-red-600">Products page</h1> 
+            <Button>ok</Button>
+        </div>
+
+    );
+};
+ 
+export default ProductPage;
