063e7cf40e879d010e2feabafb12cd289f01d47b
Natalya Nunes <nunes.natalya23@gmail.com>
2025-02-23 19:05:12 -0300

    feat: page product details

--- src/app/[slug]/menu/[productId]/page.tsx
+++ src/app/[slug]/menu/[productId]/page.tsx
@@ -2,25 +2,38 @@ import { notFound } from "next/navigation";
 
 import { db } from "@/lib/prisma";
 
+import ProductDetails from "./components/product-details";
 import ProductHeader from "./components/products-header";
-
 interface ProductPageProps{
     params: Promise<{ slug: string; productId: string }>;
 }
 
 const ProductsPage = async ({ params }: ProductPageProps) => {
     const { slug, productId } = await params;
-    const product = await db.product.findUnique({ where: { id: productId } });
+    const product = await db.product.findUnique({ 
+        where: { id: productId },
+           include:{
+              restaurant: {
+                select:{
+                    name: true,
+                    avatarImageUrl: true,
+                    slug: true,
+                },
+              },
+           }, 
+    });
     if (!product){
         return notFound();
     }
+    if(product.restaurant.slug.toUpperCase() != slug.toUpperCase()){
+        return notFound();
+    }
     return ( 
-        <>
-        <div className="relative w-full h-[300px]">
-            <ProductHeader product={product}/>
-        </div>
-        </>
+        <div className="flex h-full flex-col">
+            <ProductHeader product={product} />
+            <ProductDetails product={product} />
+        </div>     
      );
-}
+};
  
 export default ProductsPage;
